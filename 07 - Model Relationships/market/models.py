11f4e1d0dc5647b955b170fdd8c3b461b310b7c9
jsc <jimdevops@gmail.com>
2020-12-20 15:34:47 +0200

    Code after the seventh episode of Flask Series

---
+++ 07 - Model Relationships/market/models.py
@@ -0,0 +1,19 @@
+from market import db
+
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(length=30), nullable=False, unique=True)
+    email_address = db.Column(db.String(length=50), nullable=False, unique=True)
+    password_hash = db.Column(db.String(length=60), nullable=False)
+    budget = db.Column(db.Integer(), nullable=False, default=1000)
+    items = db.relationship('Item', backref='owned_user', lazy=True)
+
+class Item(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=False, unique=True)
+    owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
+    def __repr__(self):
+        return f'Item {self.name}'
---
+++ 07 - Model Relationships/market/routes.py
@@ -0,0 +1,13 @@
+from market import app
+from flask import render_template
+from market.models import Item
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+@app.route('/market')
+def market_page():
+    items = Item.query.all()
+    return render_template('market.html', items=items)
