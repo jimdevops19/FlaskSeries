06a47e00f3389b923c55859545721eba4eb19ddc
jsc <jimdevops@gmail.com>
2020-12-26 18:36:46 +0200

    Deleting wrong messaged folder

--- 09 - Flask Validations/market/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
-app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
-db = SQLAlchemy(app)
-from market import routes
--- 09 - Flask Validations/run.py
+++
@@ -1,5 +0,0 @@
-from market import app
-
-#Checks if the run.py file has executed directly and not imported
-if __name__ == '__main__':
-    app.run(debug=True)
