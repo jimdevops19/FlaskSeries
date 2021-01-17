9f7cb1e46b3f11ece3c8565b64dae4b6569d3d37
jsc <jimdevops@gmail.com>
2021-01-17 16:59:31 +0200

    Code for the Item Purchasing

---
+++ 15 - Item Purchasing Part 2/market/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
+from market import routes
